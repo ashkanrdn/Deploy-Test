56bc888017c760a8c0375219e59b6568a3a23532
ashkanrdn <41102393+ashkanrdn@users.noreply.github.com>
2021-03-06 14:44:11 -0500

    code comments

--- server/public/scripts/socketScripts.js
+++ server/public/scripts/socketScripts.js
@@ -11,27 +11,39 @@ Array.prototype.forEach.call(sliders, (slider) => {
     });
 });
 
+//selecting all the inputs
 let inputs = document.querySelectorAll('input');
 
+// making the emitter object
 let emitter = {};
 
+//looping through inputs and separating the checkbox and range
 Array.prototype.forEach.call(inputs, (input) => {
+    // every time user clicks on any input =>
     input.addEventListener('click', (event) => {
+        // we check for the input type=>
         if (event.target.attributes.type.value === 'checkbox') {
+            // if it is checkbox we get checked value
             let controlId = event.target.id;
             let controlValue = event.target.checked;
+            // then add the input ID and it value to emitter object
             Object.assign(emitter, {
                 [controlId]: controlValue,
             });
         } else if (event.target.attributes.type.value === 'range') {
+            // if it is range
             let controlId = event.target.id;
             let controlValue = event.target.value;
+            // we assign number value
             Object.assign(emitter, {
                 [controlId]: controlValue,
             });
         }
+        // whenever any changes happen we emit new message
+        // before emitting the object we need stringy it
         stringEmit = JSON.stringify(emitter);
         console.log(stringEmit, 'emit');
+        // custom message and values are now being emitted
         socket.emit('rangeChanged', stringEmit);
     });
 });
