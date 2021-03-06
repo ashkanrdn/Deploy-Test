94223575ebee09442f5fd65faefa9ee9a15d31c5
ashkanrdn <41102393+ashkanrdn@users.noreply.github.com>
2021-03-06 14:37:48 -0500

    script now gets values

--- server/public/scripts/socketScripts.js
+++ server/public/scripts/socketScripts.js
@@ -1,20 +1,12 @@
 var socket = io.connect('/');
 
-// };
-// document.getElementById('myonoffswitch').addEventListener('change', function() {
-//     socket.emit('stateChanged', this.checked);
-//     console.log(this.checked);
-// });
-// document.getElementById('myRange').addEventListener('click', function() {
-//     socket.emit('rangeChanged', this.value);
-//     console.log(this.value);
-// });
+// Show the dim value in the Dash
 
-let sliders = document.querySelectorAll('.sliderDom');
-
+let sliders = document.querySelectorAll('.sliderDom'); //getting all the slider divs
 Array.prototype.forEach.call(sliders, (slider) => {
+    //looping through divs and getting the sliders
     slider.querySelector('input').addEventListener('click', (event) => {
-        // 1. apply our value to the span
+        //   apply  value to the span
         slider.querySelector('span').innerHTML = event.target.value;
     });
 });
@@ -25,26 +17,19 @@ let emitter = {};
 
 Array.prototype.forEach.call(inputs, (input) => {
     input.addEventListener('click', (event) => {
-        if (event.target.value) {
-            // socket.emit('rangeChanged',{event.value,} );
-
+        if (event.target.attributes.type.value === 'checkbox') {
             let controlId = event.target.id;
-            let controlValue = event.target.value;
-
+            let controlValue = event.target.checked;
             Object.assign(emitter, {
                 [controlId]: controlValue,
             });
-
-            console.log(emitter, 'emit');
-        } else {
+        } else if (event.target.attributes.type.value === 'range') {
             let controlId = event.target.id;
-            let controlValue = event.target.checked;
-
+            let controlValue = event.target.value;
             Object.assign(emitter, {
                 [controlId]: controlValue,
             });
         }
-
         stringEmit = JSON.stringify(emitter);
         console.log(stringEmit, 'emit');
         socket.emit('rangeChanged', stringEmit);
