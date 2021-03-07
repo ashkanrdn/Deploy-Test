5da5e217602a6345f5ce300387f3d3e432ca1aab
ashkanrdn <41102393+ashkanrdn@users.noreply.github.com>
2021-03-07 13:31:45 -0500

    ds

--- BackendProto/test.py
+++ BackendProto/test.py
@@ -1,35 +1,35 @@
 import gpiozero
 import time
 
-
+# Test
+hello = 55555
 test = gpiozero.OutputDevice(21)
 
 delay = 0.0000001
 
-#dir = gpiozero.OutputDevice(21)
-#dir.on()
-time.sleep(.1)
+# dir = gpiozero.OutputDevice(21)
+# dir.on()
+time.sleep(0.1)
 for i in range(1600):
-    
+
     test.on()
     time.sleep(0.0000025)
-   
+
     test.off()
 
     time.sleep(delay)
 
 dir.off()
-time.sleep(.1)
+time.sleep(0.1)
 for i in range(1600):
-    
+
     test.on()
     time.sleep(0.0000025)
-   
+
     test.off()
 
     time.sleep(delay)
-    #break
-   
+    # break
 
     # for i in range(6400):
     #     time.sleep(delay)
