b24ed5d2f7b9c8e59922d02b667dad16f8e57a1c
ashkanrdn <41102393+ashkanrdn@users.noreply.github.com>
2021-03-11 03:09:30 -0500

    interface update

--- server/public/scripts/socketScripts.js
+++ server/public/scripts/socketScripts.js
@@ -12,31 +12,32 @@ let emitter = {
 };
 
 // getting the divs with controlsItemInner this is a div that has controls for each control module
-let controlItemInner = document.querySelectorAll('.controlsItemInner');
+let controlItemInner = document.querySelectorAll('.controlsItemInner.lightingCtrl');
 
 Array.prototype.forEach.call(controlItemInner, (div) => {
     //looping through divs and getting the sliders
-    div.querySelector('input[type=range]').addEventListener('click', (event) => {
-        // Showing the Dim Value
-        div.querySelector('span').innerHTML = event.target.value;
+    if (div.querySelector('input[type=range]') !== null) {
+        div.querySelector('input[type=range]').addEventListener('click', (event) => {
+            // Showing the Dim Value
+            div.querySelector('span').innerHTML = event.target.value;
 
-        // getting the ID of the control by it's classname
-        let controlId = event.target.className;
-        // remapping the value to be between 0 - 1
-        let controlValue = event.target.value * 0.01;
-        // we make the check box off if the dimmer is 0 and off it is not
-        div.querySelector('input[type=checkbox]').checked = event.target.value < 1 ? false : true;
+            // getting the ID of the control by it's classname
+            let controlId = event.target.className;
+            // remapping the value to be between 0 - 1
+            let controlValue = event.target.value * 0.01;
+            // we make the check box off if the dimmer is 0 and off it is not
+            div.querySelector('input[type=checkbox]').checked = event.target.value < 1 ? false : true;
 
-        let dimval = event.target.value < 1 ? 0 : 100;
-        // The message object
-        Object.assign(emitter, {
-            [controlId]: controlValue,
+            let dimval = event.target.value < 1 ? 0 : 100;
+            // The message object
+            Object.assign(emitter, {
+                [controlId]: controlValue,
+            });
+
+            // custom message and values are now being emitted
+            socket.emit('rangeChanged', JSON.stringify(emitter));
         });
-
-        // custom message and values are now being emitted
-        socket.emit('rangeChanged', JSON.stringify(emitter));
-    });
-
+    }
     let doubleToggle = div.querySelectorAll('input[type=checkbox]');
     Array.prototype.forEach.call(doubleToggle, (toggle) => {
         toggle.addEventListener('click', (event) => {
@@ -45,13 +46,13 @@ Array.prototype.forEach.call(controlItemInner, (div) => {
 
             let dimval = event.target.checked ? 1 : 0;
 
-            if (controlId !== 'LEDGrowMainPwr') {
 
-                console.log(controlId);
-                div.querySelector('input[type=range]').value = event.target.checked ? 100 : 0;
 
-                div.querySelector('span').innerHTML = dimval * 100;
-            }
+            console.log(controlId);
+            div.querySelector('input[type=range]').value = event.target.checked ? 100 : 0;
+
+            div.querySelector('span').innerHTML = dimval * 100;
+
             Object.assign(emitter, {
                 [controlId]: dimval,
             });
@@ -78,3 +79,62 @@ LEDMain.addEventListener('change', (toggle) => {
     }
 
 })
+
+
+
+
+//////////////////////////////////////////////////////// Swing Arm \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// getting the divs with controlsItemInner this is a div that has controls for each control module
+let controlItemInnerSwing = document.querySelectorAll('.controlsItemInner.swingCtrl');
+
+Array.prototype.forEach.call(controlItemInner, (div) => {
+    //looping through divs and getting the sliders
+    if (div.querySelector('input[type=range]') !== null) {
+        div.querySelector('input[type=range]').addEventListener('click', (event) => {
+            // Showing the Dim Value
+            div.querySelector('span').innerHTML = event.target.value;
+
+            // getting the ID of the control by it's classname
+            let controlId = event.target.className;
+            // remapping the value to be between 0 - 1
+            let controlValue = event.target.value * 0.01;
+
+            // The message object
+            Object.assign(emitter, {
+                [controlId]: controlValue,
+            });
+
+            // custom message and values are now being emitted
+            socket.emit('rangeChanged', JSON.stringify(emitter));
+        });
+    }
+    let doubleToggle = div.querySelectorAll('input[type=checkbox]');
+    Array.prototype.forEach.call(doubleToggle, (toggle) => {
+        toggle.addEventListener('click', (event) => {
+            // getting the ID of the control by it's classname
+            let controlId = event.target.className.split(' ')[0];
+
+            let dimval = event.target.checked ? 1 : 0;
+
+
+
+            console.log(controlId);
+            div.querySelector('input[type=range]').value = event.target.checked ? 100 : 0;
+
+            div.querySelector('span').innerHTML = dimval * 100;
+
+            Object.assign(emitter, {
+                [controlId]: dimval,
+            });
+
+            // custom message and values are now being emitted
+            socket.emit('rangeChanged', JSON.stringify(emitter));
+        });
+    });
+
+
+
+});
+
+
