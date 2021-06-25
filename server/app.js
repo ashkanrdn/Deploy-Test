a4d749ec41bbfa45b9bd2fd7935aa56fd74e93aa
ashkanrdn <41102393+ashkanrdn@users.noreply.github.com>
2021-06-24 22:08:34 -0400

    Master merge

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var routes = require('./routes/index');
+
+// App setup
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+// Routes setup
+app.use('/', routes);
+
+
+
+module.exports = app;
