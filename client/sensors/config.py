23610898f99861f4b8c8d69e60cf55a30b21376c
Sharaa_re <norouzi.sharare@gmail.com>
2022-03-03 17:00:40 -0500

    add mongo dataaccess

--- client/sensors/config.py
+++ client/sensors/config.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+import certifi
+
+
 class SensorsTSLs(Enum):
     AIR_1 = 0
     AIR_2 = 1
@@ -8,4 +11,6 @@ class SensorsTSLs(Enum):
     VOC = 4
 
 
-
+MONGO_CONFIGURATION = {
+    "HOST": "mongodb+srv://subham:1234@cluster0.t4iwt.mongodb.net/testdb?retryWrites=true&w=majority",
+    "tlsCAFile": certifi.where()}
