52738c5508d3d2dad3c2ec8afd493e3c9b4293d4
jefferson <jellinge@uncc.edu>
2022-04-11 12:08:08 -0400

    add prototype tests interface

---
+++ prototype/tests/sensor_tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+class SensorTest(unittest.TestCaes):
+    def setUp():
+        pass 
+    
+    def test_air_sensors_output():
+        pass
+
+    def test_soil_sensors_output():
+        pass
+
+    def test_co2_sensors_output():
+    pass
+
+    def test_voc_sensors_output():
+    pass
+
+    def test_light_sensors_output():
+    pass
